19c2c33f5c4461bd44938b7282f5d76a82ee0dfc
Lia <lia.tabilo@gmail.com>
2019-09-15 19:19:40 +0100

    Adjust card 2

--- src/Card2.js
+++ src/Card2.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import JapaneseMobile from './images/JapaneseMobile.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import './Card2.css';
@@ -8,7 +7,7 @@ class Card2 extends Component {
 	render() {
 		return (
 			<div>
-				<img className="jpmobile" src={JapaneseMobile} alt="Japanese Mobile" />
+				<img className="jpmobile" src="https://i.imgur.com/gACoce3.png" alt="Japanese Mobile" />
 				<FontAwesomeIcon icon={faPlusCircle} size="1x" className="plusIcon2" />
 			</div>
 		);
