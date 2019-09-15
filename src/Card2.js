38cec2b5902f349e169d03b3b684efd331f52a5d
Lia <lia.tabilo@gmail.com>
2019-09-15 18:50:16 +0100

    Replace background img card 3

--- src/Card2.js
+++ src/Card2.js
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
-import JapaneseMobile from "./images/JapaneseMobile.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-import "./Card2.css";
+import React, { Component } from 'react';
+import JapaneseMobile from './images/JapaneseMobile.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import './Card2.css';
 
 class Card2 extends Component {
-  render() {
-    return (
-      <div>
-        <img
-          className="mobile"
-          src={JapaneseMobile}
-          alt=""
-          className="jpmobile"
-        />
-        <FontAwesomeIcon icon={faPlusCircle} size="1x" className="plusIcon2" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<img className="jpmobile" src={JapaneseMobile} alt="Japanese Mobile" />
+				<FontAwesomeIcon icon={faPlusCircle} size="1x" className="plusIcon2" />
+			</div>
+		);
+	}
 }
 
 export default Card2;
--- src/Card3.js
+++ src/Card3.js
@@ -1,38 +1,41 @@
-import React, { Component } from "react";
-import JapaneseFood from "./images/JapaneseFood.png";
-import Draggable from "react-draggable";
-
-import "./Card3.css";
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+import './Card3.css';
 
 class Card3 extends Component {
-  render() {
-    return (
-      <div
-        className="parent"
-        style={{
-          height: "500px",
-          width: "290px",
-          position: "relative",
-          overflow: "auto",
-          padding: "0"
-        }}
-      >
-        <div style={{ height: "1000px", padding: "0" }}>
-          <Draggable
-            bounds={(0, 10, 0, 10)}
-            handle=".handle"
-            grid={(25, 25)}
-            allowAnyClick={true}
-            cancel={".body"}
-            disabled={true}
-            dragHandlers={false}
-          >
-            <img src={JapaneseFood} alt="" className="backgroundImg" />
-          </Draggable>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div
+				className="parent"
+				style={{
+					height: '500px',
+					width: '275px',
+					position: 'relative',
+					overflow: 'auto',
+					padding: '0',
+				}}
+			>
+				<div style={{ height: '500px', width: '60px', padding: '0' }}>
+					<Draggable
+						bounds={(0, 10, 0, 10)}
+						handle=".handle"
+						grid={(25, 15)}
+						allowAnyClick={true}
+						cancel={'.body'}
+						disabled={true}
+						dragHandlers={false}
+						overflow="auto"
+					>
+						<img
+							src="https://i.imgur.com/fxW6FGg.jpg"
+							alt="Japanese Background"
+							className="backgroundImg"
+						/>
+					</Draggable>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Card3;
