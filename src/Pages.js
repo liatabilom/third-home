b1da50444bfaa2abe978c259f5ab800cdf7ddca4
Lia <lia.tabilo@gmail.com>
2020-02-29 16:40:04 +0000

    Update pages.js

--- src/Pages.js
+++ src/Pages.js
@@ -5,18 +5,18 @@ class Pages extends Component {
 	render() {
 		return (
 			<div className="bottom-pages">
-				<button className="page-button" href="https://www.correiagoncalves.com">
+				<a className="page-button active" href="https://www.correiagoncalves.com">
 					Page 1
-				</button>
-				<button className="page-button" href="https://home2.correiagoncalves.com">
+				</a>
+				<a className="page-button" href="https://home2.correiagoncalves.com">
 					Page 2
-				</button>
-				<button className="page-button active" href="https://home3.correiagoncalves.com/">
+				</a>
+				<a className="page-button" href="https://home3.correiagoncalves.com/">
 					Page 3
-				</button>
-				<button className="page-button" href="https://home4.correiagoncalves.com">
+				</a>
+				<a className="page-button" href="https://home4.correiagoncalves.com">
 					Page 4
-				</button>
+				</a>
 			</div>
 		);
 	}
